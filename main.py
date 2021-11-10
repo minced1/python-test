f004034e8be15fefae59d150aca7a7dee353d535
minced1 <66805829+minced1@users.noreply.github.com>
2021-11-10 16:50:36 +0100

    fix

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from random import seed
 from random import randint
+name, insultI
 
 insults = ["Idiot", "Arsch", "Spast", "Diggasagender"]
 
 def insultPerson(name, insultId):
     return(name + ", du " + insults[insultId])
-
-insultPerson()
+    
 print(insultPerson("Tim", randint(0, len(insults))))
