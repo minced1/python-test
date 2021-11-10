d31ac708055ac273d8b4a50fc55315b19aa32708
minced1 <66805829+minced1@users.noreply.github.com>
2021-11-10 17:06:49 +0100

    added input for name

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import random
 
+name = str(input("Wen willst du beleidigen?"))
 insults = ["Idiot", "Arsch", "Spast", "Diggasagender"]
 
 def insultPerson(name, insultId):
     return(name + ", du " + insults[insultId])
     
-print(insultPerson("Tim", random.randint(0, len(insults)-1)))
+print(insultPerson(name, random.randint(0, len(insults)-1)))
